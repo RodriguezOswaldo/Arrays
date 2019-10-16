4554e80fe71a59afaa4136f8270f000108963df3
RodriguezOswaldo <owner2303@gmail.com>
2019-10-16 13:25:34 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new[] {3, 5, 7, 9, 2, 15, 12};
+            //Length
+            Console.BackgroundColor = ConsoleColor.DarkMagenta;
+            Console.WriteLine("Length: " + numbers.Length);
+            
+            //Index
+            var index = Array.IndexOf(numbers, 9);
+            Console.WriteLine("index of 9: "+ index);
+
+            //Clear
+            Array.Clear(numbers, 0, 2);
+            foreach (var n in numbers)
+            {
+                Console.WriteLine(n);
+            }
+
+            //copy
+            int[] another = new int[3];
+            Array.Copy(numbers, another, 3);
+
+            Console.WriteLine("Effect of copy()");
+            foreach (var n in another)
+                Console.WriteLine(n);
+          
+
+            //Sort
+            Array.Sort(numbers);
+            Console.WriteLine("Effect of Sort()");
+            foreach (var sort in numbers)
+            {
+                Console.WriteLine(sort);
+            }
+
+            //Reverse
+            Array.Reverse(numbers);
+
+            Console.WriteLine("Effect of Reverse(): ");
+            foreach (var reverse in numbers)
+                Console.WriteLine(reverse);
+            {
+                
+            }
+
+        }
+    }
+}
